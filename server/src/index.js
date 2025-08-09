a522e3b5e6dacdee27567337d05f2ef0bfb40dab
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-08-09 22:56:26 +0300

    fixes db

--- server/src/index.js
+++ server/src/index.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`Try to visit http://localhost:${PORT}/api/health`);
 });
